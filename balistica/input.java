6b4a7dbefeaf659cae64cc5adc6409033a6c2751
Celsozne <celso.machado@etec.sp.gov.br>
2024-04-10 01:57:21 -0300

    update
    mais algum avanco

---
+++ balistica/input.java
@@ -0,0 +1,9 @@
+package balistica;
+
+import javax.swing.*;
+
+public class input {
+    private JTextField tx_v0;
+    private JLabel teta;
+    private JTextField txteta;
+}
---
+++ balistica/movimento.java
@@ -0,0 +1,9 @@
+package balistica;
+
+public class movimento{
+    void movimentoX(){
+        for (double t0= 0 ;t0<=100; t0+=0.1) {
+            double x =
+        }
+    }
+}
